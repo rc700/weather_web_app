273aef06f35e01dd1343458465cae2121d7a62ca
Rachael <rcoulter700@googlemail.com>
2017-07-04 10:47:18 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ const axios = require('axios');
 
 const app = express();
 
-const api_url = 'http://api.openweathermap.org/data/2.5/weather?q=Belfast,uk&APPID=c29c705123528d7ad5585d0e256486c7'
+const api_url = 'Belfast,uk'
+const url_1 = 'http://api.openweathermap.org/data/2.5/weather?q='
+const url_2 = '&APPID=c29c705123528d7ad5585d0e256486c7'
 
 app.use(express.static('static'));
 
 app.get('/weather', function(req, res){
-    axios.get(api_url).then(function(response){
+    var city = req.query.city;
+    axios.get(url_1 + city + url_2).then(function(response){
         res.send(response['data'])
     })
 });
